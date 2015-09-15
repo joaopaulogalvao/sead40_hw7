d7923478b886de585c4befd4c6ada81e6d06910c
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-15 09:50:17 -0700

    First commit.

---
+++ sead40_hw7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/15/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sead40_hw7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/15/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
