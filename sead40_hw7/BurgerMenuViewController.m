8f648e209e6fcd464f3b3f7109435c68acaaa3c4
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-15 11:38:01 -0700

    Add question search logic and put it at the top.

--- sead40_hw7/BurgerMenuViewController.m
+++ sead40_hw7/BurgerMenuViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BurgerMenuViewController.h"
+#import "QuestionSearchViewController.h"
 
 CGFloat const kburgerOpenScreenDivider = 3.0;
 CGFloat const kburgerOpenScreenMultiplier = 2.0;
@@ -16,6 +17,11 @@ CGFloat const kburgerButtonHeight = 50.0;
 
 @interface BurgerMenuViewController ()<UITableViewDelegate>
 
+@property (strong,nonatomic) UIViewController *topViewController;
+@property (strong,nonatomic) UIButton *burgerButton;
+@property (strong,nonatomic) UIPanGestureRecognizer *pan;
+@property (strong,nonatomic) NSArray *viewControllers;
+
 @end
 
 @implementation BurgerMenuViewController
@@ -24,13 +30,42 @@ CGFloat const kburgerButtonHeight = 50.0;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
   
+  //Instantiate the main menu
   UITableViewController *mainMenuViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"MainMenu"];
   mainMenuViewController.tableView.delegate = self;
   
+  //Add it as a child view controller
   [self addChildViewController:mainMenuViewController];
   mainMenuViewController.view.frame = self.view.frame;
   [self.view addSubview:mainMenuViewController.view];
   [mainMenuViewController didMoveToParentViewController:self];
+  
+  //Instantiate the question search
+  QuestionSearchViewController *questionSearchVC = [self.storyboard instantiateViewControllerWithIdentifier:@"QuestionSearch"];
+  
+  //Instantiate my questions view
+  
+  //MyQuestionsViewController *myQuestionsVC = [self.storyboard instantiateViewControllerWithIdentifier:@"MyQuestions"];
+  
+  
+  //Add both views to a view controllers array
+  self.viewControllers = @[questionSearchVC];
+  
+  //Add question search as a child of the burger view controller
+  [self addChildViewController:questionSearchVC];
+  
+  //Set question search's frame
+  questionSearchVC.view.frame = self.view.frame;
+  
+  //Add question search view as a subview of the burger view Controller
+  [self.view addSubview:questionSearchVC.view];
+  
+  //Inform the delegate that question Search moved to its parent view controller
+  [questionSearchVC didMoveToParentViewController:self];
+  
+  //Make question search the view at the top
+  self.topViewController = questionSearchVC;
+  
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ sead40_hw7/QuestionSearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuestionSearchViewController.m
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/15/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import "QuestionSearchViewController.h"
+
+@interface QuestionSearchViewController ()
+
+@end
+
+@implementation QuestionSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
