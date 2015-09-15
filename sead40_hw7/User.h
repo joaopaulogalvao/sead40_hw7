3d43b9fde62ac963e7d0954218ac83ca36efede3
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-15 10:36:27 -0700

    Add User model and delete ViewController.

---
+++ sead40_hw7/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/15/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface User : NSObject
+
+@property(strong, nonatomic) NSString *username;
+@property(strong, nonatomic) UIImage *profileImage;
+
+@end
--- sead40_hw7/User.m
+++ sead40_hw7/User.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  User.m
 //  sead40_hw7
 //
 //  Created by Joao Paulo Galvao Alves on 9/15/15.
 //  Copyright (c) 2015 jalvestech. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "User.h"
 
-@interface ViewController : UIViewController
-
+@implementation User
 
 @end
-
