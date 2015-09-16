d9a85e0b7ca6625708c258e2d9de8592efc53663
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-16 09:15:54 -0700

    Add MyprofileViewController.

---
+++ sead40_hw7/MyProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyProfileViewController.h
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/16/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyProfileViewController : UIViewController
+
+@end
--- sead40_hw7/WebViewController.m
+++ sead40_hw7/WebViewController.m
@@ -8,7 +8,7 @@
 
 #import "WebViewController.h"
 
-@interface WebViewController ()
+@interface WebViewController ()<WKNavigationDelegate>
 
 @end
 
@@ -16,12 +16,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  // Create the webview
+  WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+  [self.view addSubview:webView];
+  
+  // Make it the delegate of this viewController
+  webView.navigationDelegate = self;
+  
+  //Open a new window with client ID
+  NSString *baseURL = @"https://stackexchange.com/oauth/dialog";
+  NSString *clientID = @"5570";
+  NSString *redirectURI = @"https://stackexchange.com/oauth/login_success";
+  NSString *finalURL = [NSString stringWithFormat:@"%@?client_id=%@&redirect_uri=%@",baseURL,clientID,redirectURI];
+  [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:finalURL]]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - WKNavigationDelegate
+-(void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+  
+  NSLog(@"%@",navigationAction.request.URL.description);
+  
+  if ([navigationAction.request.URL.path isEqualToString:@"/oauth/login_success"]) {
+    NSString *fragmentString = navigationAction.request.URL.fragment;
+    NSArray *components = [fragmentString componentsSeparatedByString:@"&"];
+    NSString *fullTokenParameter = components.firstObject;
+    NSString *token = [fullTokenParameter componentsSeparatedByString:@"="].lastObject;
+    NSLog(@"%@",token);
+    [[NSUserDefaults standardUserDefaults] setObject:token forKey:@"token"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self dismissViewControllerAnimated:NO completion:nil];
+    
+  }
+  
+  decisionHandler(WKNavigationActionPolicyAllow);
+  
 }
 
 /*
