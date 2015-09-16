6661ddb4ad839ec46f1aa78801c62d1d1b186269
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-16 10:21:48 -0700

    Add QuestionJSONParser and constraints to search bar.

---
+++ sead40_hw7/QuestionJSONParser.m
@@ -0,0 +1,13 @@
+//
+//  QuestionJSONParser.m
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/16/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import "QuestionJSONParser.h"
+
+@implementation QuestionJSONParser
+
+@end
