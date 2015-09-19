617e890d2a9d52683582462e6cd9ba84deea683e
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-18 19:14:12 -0700

    Add MyQuestionsTableViewCell outlets.

---
+++ sead40_hw7/MyQuestionsTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MyQuestionsTableViewCell.h
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/18/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyQuestionsTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgViewMyQuestions;
+@property (weak, nonatomic) IBOutlet UILabel *labelMyQuestions;
+@property (weak, nonatomic) IBOutlet UILabel *labelMyQuestionsText;
+
+@end
---
+++ sead40_hw7/MyQuestionsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyQuestionsTableViewCell.m
+//  sead40_hw7
+//
+//  Created by Joao Paulo Galvao Alves on 9/18/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import "MyQuestionsTableViewCell.h"
+
+@implementation MyQuestionsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
